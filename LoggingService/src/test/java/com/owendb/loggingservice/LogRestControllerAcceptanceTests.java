7942afae735a4aa2acdfcee42d4c2ebae4b1089a
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-12-13 16:19:41 +0100

    Fixed the acceptance test for the microservice

--- LoggingService/src/test/java/com/owendb/loggingservice/LogRestControllerAcceptanceTests.java
+++ LoggingService/src/test/java/com/owendb/loggingservice/LogRestControllerAcceptanceTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-acceptancetest.properties")
 class LogRestControllerAcceptanceTests {
 
     @Autowired
@@ -21,7 +23,7 @@ class LogRestControllerAcceptanceTests {
 
     @Test
     void shouldGetActionLogs() throws Exception {
-        mockMvc.perform(get("api/log"))
+        mockMvc.perform(get("/api/log"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
