da199741fd94630077e7f5f9d7a4f46ea9bfc053
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-12-13 18:42:51 +0100

    Removed security filter from test

--- LoggingService/src/test/java/com/owendb/loggingservice/LogRestControllerAcceptanceTests.java
+++ LoggingService/src/test/java/com/owendb/loggingservice/LogRestControllerAcceptanceTests.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @TestPropertySource(locations = "classpath:application-acceptancetest.properties")
 class LogRestControllerAcceptanceTests {
 
